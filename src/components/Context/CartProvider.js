a3bd84551aab55262f6caa019a57583e2c6e2b88
roshani2022 <rosh.gupta17@gmail.com>
2023-12-09 00:35:00 +0530

    add item handler work

--- src/components/Context/CartProvider.js
+++ src/components/Context/CartProvider.js
@@ -18,30 +18,27 @@ const CartProvider = (props) => {
       let userId = updatedItems[existingItemIndex]._id;
 
       try {
-        const res = await axios.put(
-          `https://crudcrud.com/api/8be34c0022ba4f5eac7de22e3b00f13c/cart${userEmail}/${userId}`,
+        await axios.put(
+          `https://crudcrud.com/api/a886f93c2d2449418ffc77b3f12e1fb2/cart${userEmail}/${userId}`,
           {
             ...item,
             quantity: Number(updatedItems[existingItemIndex].quantity),
           }
         );
-        setItems(res.data);
-        // Set the state after updating the item in the array
+
+        setItems(updatedItems);
       } catch (err) {
         console.log(err);
       }
-      // setItems(updatedItems);
     } else {
-      // setItems((prevItems) => [...prevItems, { ...item, quantity: 1 }]);
       try {
         const response = await axios.post(
-          `https://crudcrud.com/api/8be34c0022ba4f5eac7de22e3b00f13c/cart${userEmail}`,
-          {
-            ...item,quantity:1
-          }
-          
+          `https://crudcrud.com/api/a886f93c2d2449418ffc77b3f12e1fb2/cart${userEmail}`,
+
+          item
         );
-        setItems([response.data]);
+
+        setItems((prevItems) => [...prevItems, response.data]);
       } catch (err) {
         console.log(err);
       }
@@ -52,7 +49,7 @@ const CartProvider = (props) => {
     const getCartItems = async () => {
       try {
         const response = await axios.get(
-          `https://crudcrud.com/api/8be34c0022ba4f5eac7de22e3b00f13c/cart${userEmail}`
+          `https://crudcrud.com/api/a886f93c2d2449418ffc77b3f12e1fb2/cart${userEmail}`
         );
 
         setItems(response.data);
@@ -65,25 +62,19 @@ const CartProvider = (props) => {
 
   const removeItemFromCartHandler = async (id) => {
     const existingItemIndex = items.findIndex((cartItem) => cartItem.id === id);
-
+        console.log(id)
     if (existingItemIndex !== -1) {
-      // If the item exists in the cart, get its current quantity
       const currentItem = items[existingItemIndex];
+      const userId = currentItem._id;
 
-      let userId = currentItem._id;
-
-      console.log("userId", userId);
-
-      // If the quantity is greater than 1, decrement it by 1
       if (currentItem.quantity > 1) {
+        // If the quantity is greater than 1, decrement it by 1
         const updatedItems = [...items];
-
         updatedItems[existingItemIndex].quantity -= 1;
-        setItems((prevItems) => prevItems.filter((item) => item.id !== id));
 
         try {
           const response = await axios.put(
-            `https://crudcrud.com/api/8be34c0022ba4f5eac7de22e3b00f13c/cart${userEmail}/${userId}`,
+            `https://crudcrud.com/api/a886f93c2d2449418ffc77b3f12e1fb2/cart${userEmail}/${userId}`,
             {
               ...id,
               quantity: Number(updatedItems[existingItemIndex].quantity),
@@ -91,26 +82,31 @@ const CartProvider = (props) => {
           );
           setItems(response.data);
         } catch (err) {
-          console.log(err);
+          console.error("Error updating quantity:", err);
+          // Handle error, e.g., show a message to the user
         }
         setItems(updatedItems);
       } else {
         // If the quantity is 1, remove the item from the cart
-
         const updatedItems = items.filter((item) => item.id !== id);
+
         try {
           const response = await axios.delete(
-            `https://crudcrud.com/api/8be34c0022ba4f5eac7de22e3b00f13c/cart${userEmail}/${userId}`
+            `https://crudcrud.com/api/a886f93c2d2449418ffc77b3f12e1fb2/cart${userEmail}/${userId}`
           );
           setItems(response.data);
         } catch (err) {
-          console.log(err);
+          console.error("Error removing item from cart:", err);
+          // Handle error, e.g., show a message to the user
         }
         setItems(updatedItems);
       }
     }
   };
 
+  
+        
+  
   const cartContext = {
     items: items,
     addItem: addItemToCartHandler,
